30dc88abe2e1a99e52b209897d391d409d564973
BovineBeta <bovinefbik@foxmail.com>
2025-07-12 12:35:03 +0800

    feat: assets

---
+++ src/assets.ts
@@ -0,0 +1,9 @@
+import { Asset } from "./file.ts";
+export const bgm = new Asset(
+  "./public/bgm.a808ef.ogg",
+  "bgm.a808ef.ogg",
+);
+export const workshopLogo = new Asset(
+  "./public/Avg_bg_61_g7_pcsserverroom.png",
+  "Avg_bg_61_g7_pcsserverroom.png",
+);
