31771933b7df2a40b63566f7cec64456f53f39e5
BovineBeta <bovinefbik@foxmail.com>
2025-07-12 12:35:25 +0800

    feat: build

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { TriggerElements } from "hnxml/jsx/action";
+import { TaskElements } from "hnxml/jsx/task";
+import { ComputerElements } from "hnxml/jsx/computer";
+import { MissionElements } from "hnxml/jsx/mission";
+import { InfoElements } from "hnxml/jsx/info";
+
+declare global {
+  namespace JSX {
+    type IntrinsicElements =
+      & MissionElements
+      & InfoElements
+      & TriggerElements
+      & TaskElements
+      & ComputerElements;
+  }
+}
+
+import { Builder } from "./builder.ts";
+
+// ExtensionInfo
+import { extensionInfo } from "./info.tsx";
+Builder.fileRegister(extensionInfo);
+
+// Actions
+import { hackerScriptLauncher, startingAction } from "./actions.tsx";
+Builder.fileRegister(startingAction, hackerScriptLauncher);
+
+// Nodes
+import { delayHost, playerComp } from "./computers.tsx";
+Builder.fileRegister(delayHost, playerComp);
+
+// Missions
+import { startingMission } from "./missions.tsx";
+Builder.fileRegister(startingMission);
+
+// HackerScript
+import { hackerScriptFiles, initHackerScript } from "./hackerScritps.ts";
+for (const item of hackerScriptFiles) {
+  Builder.fileRegister(item[1]);
+}
+Builder.fileRegister(initHackerScript);
+
+// Assets
+import { bgm, workshopLogo } from "./assets.ts";
+Builder.assetsRegister(bgm, workshopLogo);
+
+await Builder.build();
